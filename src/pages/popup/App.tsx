f0fc8641c5a35493b5af45ed912e7396c63a9739
Ojesh Manandhar <ojesh.manandhar@grepsr.com>
2023-09-05 15:03:50 +0545

    Use antd

--- src/pages/popup/App.tsx
+++ src/pages/popup/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from 'antd';
+
 function App() {
   return (
-    <div className='h-96 w-96'>
+    <div className='h-96 w-96 p-4 border border-blue-500'>
       <h1 className='text-xl font-bold text-nokia'>Popup</h1>
+
+      <Button type='primary'>Press Me!</Button>
     </div>
   );
 }
