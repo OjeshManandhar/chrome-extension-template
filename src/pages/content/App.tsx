151b297c42efe05045b8abdc50c571ee89866516
Ojesh Manandhar <ojesh.manandhar@grepsr.com>
2023-09-06 10:45:05 +0545

    Remove twind

--- src/pages/content/App.tsx
+++ src/pages/content/App.tsx
@@ -2,8 +2,8 @@ import { Button } from 'antd';
 
 function App() {
   return (
-    <div className='h-96 w-96 p-4 border border-blue-500'>
-      <h1 className='text-xl font-bold text-nokia'>Popup</h1>
+    <div>
+      <h1>Popup</h1>
 
       <Button type='primary'>Press Me!</Button>
     </div>
--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -4,9 +4,6 @@ import ReactDOM from 'react-dom/client';
 // components
 import App from './App';
 
-// utils
-import { attachTwindStyle } from '@/utils/attach-twind';
-
 // env
 import ENV from '@/env_config.ts';
 
@@ -16,13 +13,12 @@ const root = document.createElement('div');
 root.id = 'crx-root';
 document.body.appendChild(root);
 
+const shadowRoot = root.attachShadow({ mode: 'open' });
+
 const rootIntoShadow = document.createElement('div');
 rootIntoShadow.id = 'shadow-root';
 
-const shadowRoot = root.attachShadow({ mode: 'open' });
 shadowRoot.appendChild(rootIntoShadow);
-
-attachTwindStyle(rootIntoShadow, shadowRoot);
 
 ReactDOM.createRoot(rootIntoShadow).render(
   <React.StrictMode>
